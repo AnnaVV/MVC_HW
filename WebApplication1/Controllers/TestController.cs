a731524d4ef84f64ce08642289a1baa4c42f6f72
Anna <anna.voloshchuk@gmail.com>
2014-07-21 20:24:57 +0300

    New project added

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /My/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddTest()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteTest()
+        {
+            return View();
+        }
+	}
+}
